210a84f90ce4249a01ba59c48c05f1e8d70ec47d
superigera <super_rigera@icloud.com>
2023-03-07 22:43:55 +0900

    環境構築 vuetify vuerouter eslint

---
+++ src/components/button/InquiryButton.vue
@@ -0,0 +1,18 @@
+<template>
+  <router-link to="/login">
+    <v-btn
+      rounded="lg"
+      color="primary"
+    >
+    問い合わせ
+    </v-btn>
+  </router-link>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,47 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Top from '@/views/TopPage.vue'
+// import RecordAnalysis from '@/views/RecordAnalysis.vue'
+import Login from '@/views/Login'
+// import Signup from '@/views/Signup'
+// import Mypage from '@/views/Mypage'
+// import Inquiry from '@/views/Inquiry'
+
+const routes = [
+    {
+        path: '/top',
+        name: 'Top',
+        component: Top
+    },
+    // {
+    //     path: '/record',
+    //     name: 'RecordAnalysis',
+    //     component: RecordAnalysis
+    // },
+    {
+        path: '/login',
+        name: 'Login',
+        component: Login
+    },
+    // {
+    //     path: '/signup',
+    //     name: 'Signup',
+    //     component: Signup
+    // },
+    // {
+    //     path: '/mypage',
+    //     name: 'Mypage',
+    //     component: Mypage
+    // },
+    // {
+    //     path: '/inquiry',
+    //     name: 'Inquiry',
+    //     component: Inquiry
+    // },
+]
+
+const router = createRouter({
+    history: createWebHistory(process.env.BASE_URL),
+    routes
+})
+
+export default router
